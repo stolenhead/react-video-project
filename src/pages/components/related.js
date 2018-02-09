1b7e4c49b745eaebb10986dd6a2348a5ed330fd3
stolenhead <andrea.palomino.ruby@gmail.com>
2018-02-08 23:43:50 -0500

    Ruta

--- src/pages/components/related.js
+++ src/pages/components/related.js
@@ -1,11 +1,11 @@
 import React from 'react';
-//import logo from '../../../dist/images/logo.c06f5e5a2ae47006c0b77be4de6a2077.png';
+import logo from '../../../images/logo.png';
 import './related.css';
 
 function Related (props) {
   return (
     <div className="Related">
-      <img src="images/logo" width={250}/>
+      <img src={logo} width={250}/>
     </div>
   )
 }
